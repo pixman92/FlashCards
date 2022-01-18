a302458f91978c6a944cfe10d081765fe090098f
pixman92 <GOtrustD@verizon.net>
2022-01-17 20:14:40 -0800

    adding push / pull data functions

---
+++ Development/firebaseJS.js
@@ -0,0 +1,59 @@
+
+// =================
+// adding Firebase to project
+import​ ​firebase​ ​from​ ​'firebase'​;
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBYD67JqdmGaAJpDyj8iLy1e5xXjB4rp-c",
+    authDomain: "flashcards-ca11e.firebaseapp.com",
+    projectId: "flashcards-ca11e",
+    storageBucket: "flashcards-ca11e.appspot.com",
+    messagingSenderId: "686642076264",
+    appId: "1:686642076264:web:94d5224c8cf76aeb4779dd"
+  };
+
+ 
+// ​const​ ​firebaseApp​ ​=​ ​firebase​.​initializeApp​(​firebaseConfig​)​;
+
+
+// ​// for database 
+// ​const​ ​db​ ​=​ ​firebaseApp​.​firestore​(​)​;
+
+// =================
+
+ 
+​window​.​onload​ ​=​ ​(​)​=>​{ 
+    ​  ​init​(​)​; 
+    ​  ​collectionName​ ​=​ ​'flashCards'​; 
+​}​; 
+​var​ ​db​; 
+​function​ ​init​(​)​{ 
+​  ​var​ ​firebaseApp​ ​=​ ​firebase​.​initializeApp​(​firebaseConfig​)​; 
+    
+​    ​db​ ​=​ ​firebaseApp​.​firestore​(​)​; 
+​}
+
+// =================
+// Firebase functions
+ 
+​function​ ​pullData​(​)​{ 
+​  ​// your standard - pulling data from Firebase 
+​  ​db​.​collection​(​collectionName​)​.​get​(​)​.​then​(​(​querySnapshot​)​ ​=>​ ​{ 
+​    ​querySnapshot​.​forEach​(​(​doc​)​ ​=>​ ​{ 
+​        ​console​.​log​(​`​${​doc​.​id​}​ => ​${​JSON​.​stringify​(​doc​.​data​(​)​)​}​`​)​; 
+​    ​}​)​; 
+​}​)​; 
+​} 
+
+​function​ ​pushData​(​data​)​{ 
+​  ​// data should be JSONstring, for beginning a data push to a new record 
+​  ​db​.​collection​(​collectionName​)​.​add​(​data​)​.​then​(​(​docRef​)​ ​=>​ ​{ 
+​      ​console​.​log​(​"Document written with ID:"​,​ ​docRef​.​id​)​; 
+​  ​}​) 
+​  ​.​catch​(​(​error​)​ ​=>​ ​{ 
+​      ​console​.​error​(​"Error adding document: "​,​ ​error​)​; 
+​  ​}​)​; 
+​} 
+
+​// =================
+
