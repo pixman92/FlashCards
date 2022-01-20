0b0cd17d9960fcf02061c2d565463efd649e72c6
pixman92 <GOtrustD@verizon.net>
2022-01-19 16:15:45 -0800

    more HTML / JS intergratoin

--- Development/foregroundHTMLScript.js
+++ Development/foregroundHTMLScript.js
@@ -9,5 +9,19 @@ function hide() {
 function show(classIt) {
     document.querySelector("." + classIt).style.display = 'block';
 }
- 
 
+
+
+async function makeDeckNameHTML(){
+    // MAKE Sure to pull myJSONFlCardPULLED data
+
+    //pushes TITLE to HTML header
+    // await $('.deckNameText').html(myJSONFlashCardsPULLED.JSONobj.innerArray[0][0][1][1])
+    document.getElementsByClassName('deckNameText')[0].innerHTML = myJSONFlashCardsPULLED.JSONobj.innerArray[0][0][1][1];
+    console.log('myJSON',   );
+} 
+
+async function makeTagsHTML(){
+    // MAKE Sure to pull myJSONFlCardPULLED data
+    $('.TAGS').html(myJSONFlashCardsPULLED.JSONobj.innerArray[0][0][3][0][1][0]);
+}
