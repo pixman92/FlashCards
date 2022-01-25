a342550721be3cce0bad780e6afbf352896d670f
TheArborist <GOtrustD@verizon.net>
2022-01-24 22:12:03 -0800

    click and edit - for one item - working, kinda

--- Development/foregroundHTMLScript.js
+++ Development/foregroundHTMLScript.js
@@ -60,55 +60,55 @@ var tmp;
 // editDeck()
 // makeThemEditable()
 
-
+var holding1;
 function makeThemEditable(){
     
     //DeckNAME and Outside Click
 
-    var savedHeader = $('.deckHeaderEditScreen').text();
+    // var savedHeader = $('.deckHeaderEditScreen').text();
 
-    $('.deckHeaderEditScreen').on('click', (event)=>{
-        event.stopPropagation();
-        // var tmp = $('.deckHeaderEditScreen').html();
-        $('.deckHeaderEditScreen').html('<textarea class="deckNameEdit">'+savedHeader+'</textarea>')
-        tmp = $('.deckNameEdit').text();
-        // $('.deckHeaderEditScreen').on('click', );
-        $('.deckHeaderEditScreen').off(); 
+    // $('.deckHeaderEditScreen').on('click', (event)=>{
+    //     event.stopPropagation();
+    //     // var tmp = $('.deckHeaderEditScreen').html();
+    //     $('.deckHeaderEditScreen').html('<textarea class="deckNameEdit">'+savedHeader+'</textarea>')
+    //     tmp = $('.deckNameEdit').text();
+    //     // $('.deckHeaderEditScreen').on('click', );
+    //     $('.deckHeaderEditScreen').off(); 
         
-    });        
-    // =============================
-    $('body').on('click', (event) => {
-        if (!$(event.target).closest('.deckNameEdit').length) {
-            // the click occured outside '#element'
-            console.log('i ran out of the box');
-            $('.deckHeaderEditScreen').html();
-            $('.deckHeaderEditScreen').html('<div>'+ tmp + '</div>');
-            makeThemEditable();
-        }        
-        });
+    // });        
+    // // =============================
+    // $('body').on('click', (event) => {
+    //     if (!$(event.target).closest('.deckNameEdit').length) {
+    //         // the click occured outside '#element'
+    //         console.log('i ran out of the box');
+    //         $('.deckHeaderEditScreen').html();
+    //         $('.deckHeaderEditScreen').html('<div>'+ tmp + '</div>');
+    //         makeThemEditable();
+    //     }        
+    // });
 
-    // =============================
-    //TEXT and Outside Click
-    var TagTextArray = [];
+    // // =============================
+    // //TEXT and Outside Click
+    // var TagTextArray = [];
 
-    var TagTextArray = $('.tagsEditScreen').text();
+    // var TagTextArray = $('.tagsEditScreen').text();
 
 
-    $('.tagsEditScreen').on('click', (event)=>{
-        event.stopPropagation();
-        // var tmp = $('.deckHeaderEditScreen').html();
-        $('.tagsEditScreen').html('<textarea class="tags">'+TagTextArray+'</textarea>')
-    });
+    // $('.tagsEditScreen').on('click', (event)=>{
+    //     event.stopPropagation();
+    //     // var tmp = $('.deckHeaderEditScreen').html();
+    //     $('.tagsEditScreen').html('<textarea class="tags">'+TagTextArray+'</textarea>')
+    // });
 
-    $('body').on('click', (event) => {
-        if (!$(event.target).closest('.tagsEditScreen').length) {
-            // the click occured outside '#element'
-            console.log('i ran out of the box');
-            $('.tagsEditScreen').html();
-            $('.tagsEditScreen').html('<div>'+ TagTextArray + '</div>');
-            makeThemEditable();
-        }        
-        });
+    // $('body').on('click', (event) => {
+    //     if (!$(event.target).closest('.tagsEditScreen').length) {
+    //         // the click occured outside '#element'
+    //         console.log('i ran out of the box');
+    //         $('.tagsEditScreen').html();
+    //         $('.tagsEditScreen').html('<div>'+ TagTextArray + '</div>');
+    //         makeThemEditable();
+    //     }        
+    //     });
 
 
 
@@ -118,8 +118,74 @@ function makeThemEditable(){
     //     makeThemEditable();
     // });
 
+    var savedDeckNameFrontScreen = $('.deckNameText').text();           //saving text before edit
+    $('.deckNameText').on('click', (event)=>{                           //setting up DOM eventlistener
+        event.stopPropagation();
+        // var tmp = $('.deckHeaderEditScreen').html();
+        $('.deckNameText').html('<textarea class="deckNameTextArea">'+savedDeckNameFrontScreen+'</textarea>')       //transfer to NEW HTML
+        // holding1 = $('.deckNameTextArea').html();
+        // console.log('holding1', holding1);
+        $('.deckNameText').off();
+        // $('.deckHeaderEditScreen').on('click', );
+    });        
+
+    $('.deckNameTextArea').on('keypress', (event)=>{
+        deckName(event);
+    });
+    $('.TAGChange').on('keypress', (event)=>{
+        TAGS(event);
+    });
+
+    $('body').on('click', (event) => {                          
+        deckName(event);
+        TAGS(event);
+        //event for, clicking outside
+        // if (!$(event.target).closest('.deckNameTextArea').length) {                     // if click is outside
+        //     // the click occured outside '#element'
+        //     console.log('i ran out of the box');     
+        //     holding1 = $('.deckNameTextArea').val(); 
+        //     $('.deckNameText').html();                                          //reset html
+        //     // $('.deckNameText').html('<div>'+ holding1 + '</div>');
+        //     $('.deckNameText').text(holding1);
+        //     makeThemEditable();
+        // }        
+    });
+    // ==============Clicking TAG array===============
+    var holdingTAGS;
+    var savedTAGS = $('.TAGS').text();
+    $('.TAGS').on('click', (event)=>{
+        event.stopPropagation();
+
+        $('.TAGS').html('<textarea class="TAGChange">' + savedTAGS + '</textarea>');
+
+        $('.TAGS').off();
+    });
+    $('window').on('click', (event)=>{
+        
+    });
+    
+}
+function TAGS(event){
+    if (!$(event.target).closest('.TAGChange').length) {
+        holdingTAGS = $('.TAGChange').val();
+        $('.TAGS').html();
+        // $('.TAGS').html('<div>'+ holdingTAGS + '</div>');
+        $('.TAGS').text(holdingTAGS);
+        makeThemEditable();
+    }
 }
 
+function deckName(event){
+    if (!$(event.target).closest('.deckNameTextArea').length) {                     // if click is outside
+        // the click occured outside '#element'
+        console.log('i ran out of the box');     
+        holding1 = $('.deckNameTextArea').val(); 
+        $('.deckNameText').html();                                          //reset html
+        // $('.deckNameText').html('<div>'+ holding1 + '</div>');
+        $('.deckNameText').text(holding1);
+        makeThemEditable();
+    }      
+}
 
 // =============================
 // DONE - edit Deck obj info - logic works
